d015786c060e9da3cfdcb86a8dfcbc48a85955ca
namphilkim <44086525+namphilkim@users.noreply.github.com>
2022-10-05 10:57:42 +0900

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import sys
 
 def main(workdir_list):
   start_time = time.time()
-  n = len(workdir_list)
+  n = len(workdir_list) - 1
   isotype_dict = {'IGHM':0,'IGHD':1,'IGHG3':2,'IGHG1':3,'IGHA1':4,'IGHG2':5,'IGHG4':6,'IGHA2':7}
 
   isotpye_list = []
@@ -27,14 +27,14 @@ def main(workdir_list):
   
   iplib.basic_box_plot(data_list=isotype_list,
                x_labels=['M','D','G3','G1','A1','G2','G4','A2'],colors = ['b','r','g','orange','purple','brown'],
-               size_x=12,size_y=8,y_lim=1.0,y_label='Frequency',has_jitter=True,save='Isotype.png')
+               size_x=12,size_y=8,y_lim=1.0,y_label='Frequency',has_jitter=True,save=workdir_list[-1]+'Isotype.png')
 
   iplib.basic_box_plot(data_list=gini_list,
                x_labels=['M','D','G3','G1','A1','G2','G4','A2','All'],colors = ['b','r','g','orange','purple','brown'],
-               size_x=12,size_y=8,y_lim=1.0,y_label='Clonality',has_jitter=True,save='Clonality.png')
+               size_x=12,size_y=8,y_lim=1.0,y_label='Clonality',has_jitter=True,save=workdir_list[-1]+'Clonality.png')
 
   iplib.basic_box_plot(data_list=shm_list,
                x_labels=['M','D','G3','G1','A1','G2','G4','A2','All'],colors = ['b','r','g','orange','purple','brown'],
-               size_x=12,size_y=8,y_lim=50.0,y_label='Somatic mutation',has_jitter=True,save='Mutation.png')
+               size_x=12,size_y=8,y_lim=50.0,y_label='Somatic mutation',has_jitter=True,save=workdir_list[-1]+'Mutation.png')
 
 main(list(sys.argv))
